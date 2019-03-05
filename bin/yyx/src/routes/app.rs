1651515ae01b5fd6d1a5a17f24e65651e96a7b1d
Flux Xu <fluxxu@gmail.com>
2019-03-05 13:52:24 -0500

    Fix mac build.

--- bin/yyx/src/routes/app.rs
+++ bin/yyx/src/routes/app.rs
@@ -21,8 +21,7 @@ pub fn index<'a>() -> Response<'a> {
 
 #[cfg(not(target_os = "windows"))]
 fn translate_path(path: &Path) -> String {
-  let path = path.to_string_lossy();
-  path.to_string()
+  path.to_string_lossy().to_string()
 }
 
 #[cfg(target_os = "windows")]
