518fc1dca6d7fcdd13781e8168b009edd9ad5247
Flux Xu <fluxxu@gmail.com>
2019-11-15 00:49:14 -0500

    fix: fix ocr2.0 json export.

--- bin/yyx/src/routes/export.rs
+++ bin/yyx/src/routes/export.rs
@@ -4,17 +4,24 @@ use serde_json::{self, Value};
 use std::path::Path;
 use warp::{path, Filter, Rejection, Reply};
 use yyx_data::save_exported_file;
+use serde_derive::Deserialize;
+
+#[derive(Deserialize)]
+struct ExportJsonQuery {
+  filename: String
+}
 
 pub fn export_json() -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
   warp::post2()
-    .and(path!("export" / "json" / String))
+    .and(path!("export" / "json"))
+    .and(warp::filters::query::query())
     .and(warp::body::json())
-    .and_then(|name: String, value: Value| {
+    .and_then(|q: ExportJsonQuery, value: Value| {
       block(move || -> Result<_, Rejection> {
         let pretty_json = serde_json::to_string_pretty(&value as &Value)
           .map_err(YyxError::internal)
           .map_err(warp::reject::custom)?;
-        let name = save_exported_file(&name, &pretty_json)
+        let name = save_exported_file(&q.filename, &pretty_json)
           .map_err(YyxError::internal)
           .map_err(warp::reject::custom)?;
         Ok(warp::reply::json(&name))
@@ -27,7 +34,8 @@ pub fn files() -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clon
     .and(warp::fs::dir("exports"))
     .and(warp::filters::path::full())
     .map(|file, fullpath: warp::filters::path::FullPath| {
-      let path = Path::new(fullpath.as_str());
+      let path: &str = &(fullpath.as_str())[1..];
+      let path = Path::new(path);
       let name = path
         .file_name()
         .map(|name| name.to_string_lossy().to_string())
